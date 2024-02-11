658e501b5a792fffc1994cb3f127f75a05b98c22
NorimitsuSuga <149935006+michelleyagi@users.noreply.github.com>
2024-02-11 22:30:32 +0000

    コントローラー修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,6 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!,except: [:top]
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!,except: [:top], unless: :admin_controller?
   
-  def after_sign_in_path_for(resource)
-    post_images_path
-  end
   
   private
   
@@ -12,9 +8,4 @@ class ApplicationController < ActionController::Base
     self.class.module_parent_name == 'Admin'
   end
 
-  protected
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-  end
 end
