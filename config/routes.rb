db7c8804e6a0ae25f23908710dca70bea99dbde7
NorimitsuSuga <149935006+michelleyagi@users.noreply.github.com>
2024-02-11 22:08:59 +0000

    scope public追記

--- config/routes.rb
+++ config/routes.rb
@@ -5,14 +5,18 @@ Rails.application.routes.draw do
   
   namespace :admin do
     get 'dashboards', to: 'dashbords#index'
+    resources :users, only: [:destroy]
   end
   
-  root to: 'homes#top'
-  devise_for :users
-  resources :users, only: [:show, :edit, :update]
-  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
-    resource :favorites, only: [:create, :destroy]
-    resources :post_comments, only: [:create, :destroy]
+  scope module: :public do
+    devise_for :users
+    root to: 'homes#top'
+    get 'homes/about', to: 'homes#about', as: :about
+    resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+      resource :favorites, only: [:create, :destroy]
+      resources :post_comments, only: [:create, :destroy]
+    end
+    resources :users, only: [:show, :edit, :update]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
