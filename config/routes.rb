e65fad78068cc537fa9a7bf1e44d1b06e0d9a1b7
NorimitsuSuga <149935006+michelleyagi@users.noreply.github.com>
2024-02-12 02:27:39 +0000

    devise不具合修正後

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :admin, skip: [:registrations, :password], controllers: {
+  # 顧客用
+  # URL /customers/sign_in ...
+  devise_for :customers, skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+  
+  # 管理者用
+  # URL /admin/sign_in ...
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
     sessions: 'admin/sessions'
   }
   
